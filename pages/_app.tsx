8bf377525f50d1a49b0be50ad3174f2cbb124e4d
junbong <koliie9039@gmail.com>
2022-01-24 00:59:12 +0900

    update: move logic of moving to native browser to useEffect of index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Script from 'next/script'
 
 import '../public/styles/global.scss'
 
@@ -9,24 +8,6 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
-      <Script
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.onload = function() {
-              if (navigator.userAgent.match(/inapp|NAVER|KAKAOTALK|Snapchat|Line|WirtschaftsWoche|Thunderbird|Instagram|everytimeApp|WhatsApp|Electron|wadiz|AliApp|zumapp|iPhone(.*)Whale|Android(.*)Whale|kakaostory|band|twitter|DaumApps|DaumDevice\\/mobile|FB_IAB|FB4A|FBAN|FBIOS|FBSS|SamsungBrowser\\/[^1]/i)) {
-                document.body.innerHTML = '';
-  
-                if (navigator.userAgent.match(/iPhone|iPad/i)) {
-                  location.href='ftp://wedding220301.com/bridge.html?_targeturl='+location.href;
-                } else {
-                  location.href='intent://'+location.href.replace(/https?:\\/\\//i,'')+'#Intent;scheme=http;package=com.android.chrome;end';
-                }
-              }
-            }
-          `,
-        }}
-      />
       <Component {...pageProps} />
     </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,18 @@ export default function Home() {
   })
   const { ref: scrollContainerRef, width, height } = useResizeDetector()
 
+  useEffect(function moveToNativeBrowser() {
+    if (navigator.userAgent.match(/inapp|NAVER|KAKAOTALK|Snapchat|Line|WirtschaftsWoche|Thunderbird|Instagram|everytimeApp|WhatsApp|Electron|wadiz|AliApp|zumapp|iPhone(.*)Whale|Android(.*)Whale|kakaostory|band|twitter|DaumApps|DaumDevice\/mobile|FB_IAB|FB4A|FBAN|FBIOS|FBSS|SamsungBrowser\/[^1]/i)) {
+      document.body.innerHTML = '';
+
+      if (navigator.userAgent.match(/iPhone|iPad/i)) {
+        location.href='ftp://wedding220301.com/bridge.html?_targeturl='+location.href;
+      } else {
+        location.href='intent://'+location.href.replace(/https?:\/\//i,'')+'#Intent;scheme=http;package=com.android.chrome;end';
+      }
+    }
+  }, [])
+
   useEffect(function attachWindowScrollEventListener() {
     function handleScrollWindow() {
       setScrollY(window.scrollY)
